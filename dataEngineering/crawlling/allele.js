4a53bcf401fa7c6554441a758782d4f6e02abe2c
ChoWonmin <0108257@gmail.com>
2020-01-24 13:44:53 +0900

    setting tsconfig, eslintignore

---
+++ dataEngineering/crawlling/allele.js
@@ -0,0 +1,21 @@
+var Crawler = require("crawler");
+
+var c = new Crawler({
+  maxConnections: 10,
+  // This will be called for each crawled page
+  callback: function(error, res, done) {
+    if (error) {
+      console.log(error);
+    } else {
+      var $ = res.$;
+      // $ is Cheerio by default
+      //a lean implementation of core jQuery designed specifically for the server
+      console.log($("title").text());
+    }
+    done();
+  }
+});
+
+const res = c.queue("http://www.amazon.com");
+
+console.log(res);
