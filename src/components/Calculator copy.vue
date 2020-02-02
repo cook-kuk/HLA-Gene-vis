b64a4b97458b419b201d8c14bb775c925f403533
SeunghoKuk-dev <seungho@gugseunghoui-MacBook-Pro.local>
2020-02-02 15:08:06 +0900

    merge

---
+++ src/components/Calculator copy.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="calculator">
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    <div>test</div>
+    </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.calculator {
+ display : grid;
+ grid-template-columns: repeat(4, 1fr);
+ grid-auto-rows: minmax(50px, auto);
+
+}
+
+
+</style>
